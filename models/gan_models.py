881952246893a5171c25d37741ecf3c9e077e758
Arta Asadi <artaasd95@gmail.com>
2024-08-17 13:33:52 +0330

    added experiment logging added train test split

--- models/gan_models.py
+++ models/gan_models.py
@@ -63,8 +63,8 @@ class Discriminator(nn.Module):
                             num_layers=num_layers, batch_first=True, dropout=dropout_prob)
         
         # Fully connected layers
-        self.fc1 = nn.Linear(hidden_size, 64)
-        self.fc2 = nn.Linear(64, 1)
+        self.fc1 = nn.Linear(hidden_size, 128)
+        self.fc2 = nn.Linear(128, 1)
         
         # Activation functions
         self.leaky_relu = nn.LeakyReLU(0.2)
@@ -76,35 +76,9 @@ class Discriminator(nn.Module):
         
         # Fully connected layers
         out = self.leaky_relu(self.fc1(out))
-        out = self.sigmoid(self.fc2(out))
+        out = self.fc2(out)
+        #out = self.sigmoid(self.fc2(out))
         return out
 
 
 
-def discriminator_loss(real_data, fake_data, real_tail, fake_tail):
-    # Binary Cross Entropy loss for real vs fake
-    # real_loss = torch.mean(torch.log(real_data + 1e-8))
-    # fake_loss = torch.mean(torch.log(1 - fake_data + 1e-8))
-
-    real_loss = F.binary_cross_entropy(fake_data, real_data)
-    fake_loss = F.binary_cross_entropy(fake_tail, real_tail)
-    
-    # Log loss for tail distribution (checking if fake_tail data comes from a fat-tail distribution)
-    tail_loss = torch.mean((fake_tail - real_tail) ** 2)
-    
-    total_loss = - (real_loss + fake_loss) + tail_loss
-    return total_loss
-
-def generator_loss(real_data, fake_data, real_tail, fake_tail):
-    # Adversarial loss for the generators
-    #main_gen_loss = torch.mean(torch.log(fake_data + 1e-8))
-
-    main_gen_loss = F.kl_div(fake_data, real_data)
-    
-    # Tail distribution regularization for noise generator
-    #noise_gen_loss = torch.mean((fake_tail - 1) ** 2)
-
-    noise_gen_loss = F.kl_div(fake_tail, real_tail)
-    
-    total_loss = main_gen_loss + noise_gen_loss
-    return total_loss
